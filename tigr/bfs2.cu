6b909afc22f4b145389427e04e4f164939cfaf44
wpy@epcc <wpybtw@outlook.com>
2019-04-16 14:21:29 +0800

    bfs2

--- tigr/bfs2.cu
+++ tigr/bfs2.cu
@@ -7,73 +7,66 @@
 #include "../shared/argument_parsing.hpp"
 #include "../shared/gpu_error_check.cuh"
 
-
-
-
-__global__ void kernel(unsigned int numParts, 
-							unsigned int *nodePointer, 
-							PartPointer *partNodePointer,
-							unsigned int *edgeList, 
-							unsigned int *dist, 
-							bool *finished,
-							int level)
+__global__ void kernel(unsigned int numParts,
+					   unsigned int *nodePointer,
+					   PartPointer *partNodePointer,
+					   unsigned int *edgeList,
+					   unsigned int *dist,
+					   bool *finished,
+					   int level)
 {
 	unsigned int partId = blockDim.x * blockIdx.x + threadIdx.x;
 
-	if(partId < numParts)
+	if (partId < numParts)
 	{
 		unsigned int id = partNodePointer[partId].node;
 		unsigned int part = partNodePointer[partId].part;
 
-		if(dist[id] != level)
+		if (dist[id] != level)
 			return;
 
 		unsigned int thisPointer = nodePointer[id];
 		unsigned int degree = edgeList[thisPointer];
-			
+
 		unsigned int numParts;
-		if(degree % Part_Size == 0)
-			numParts = degree / Part_Size ;
+		if (degree % Part_Size == 0)
+			numParts = degree / Part_Size;
 		else
 			numParts = degree / Part_Size + 1;
 
-		
 		unsigned int end;
 
 		unsigned int ofs = thisPointer + part + 1;
 
-		for(int i=0; i<Part_Size; i++)
+		for (int i = 0; i < Part_Size; i++)
 		{
-			if(part + i*numParts >= degree)
+			if (part + i * numParts >= degree)
 				break;
-			end = ofs + i*numParts;
-			
-			if(dist[edgeList[end]] > level + 1)
+			end = ofs + i * numParts;
+
+			if (dist[edgeList[end]] > level + 1)
 			{
 				dist[edgeList[end]] = level + 1;
 				*finished = false;
 			}
 		}
-		
 	}
 }
-
 
 __global__ void clearLabel(bool *label, unsigned int size)
 {
 	unsigned int id = blockDim.x * blockIdx.x + threadIdx.x;
-	if(id < size)
+	if (id < size)
 		label[id] = false;
 }
 
-
-int main(int argc, char** argv)
-{	
+int main(int argc, char **argv)
+{
 	ArgumentParser arguments(argc, argv, true, false);
-	
+
 	Graph graph(arguments.input, false);
 	graph.ReadGraph();
-	
+
 	VirtualGraph vGraph(graph);
 	Timer t22;
 	t22.Start();
@@ -82,27 +75,26 @@ int main(int argc, char** argv)
 
 	uint num_nodes = graph.num_nodes;
 	uint num_edges = graph.num_edges;
-	
-	if(arguments.hasDeviceID)
+
+	if (arguments.hasDeviceID)
 		cudaSetDevice(arguments.deviceID);
 
 	cudaFree(0);
-	
-	unsigned int *dist;
-	dist  = new unsigned int[num_nodes];
 
-	for(int i=0; i<num_nodes; i++)
+	unsigned int *dist;
+	dist = new unsigned int[num_nodes];
+
+	for (int i = 0; i < num_nodes; i++)
 	{
 		dist[i] = DIST_INFINITY;
 	}
 	dist[arguments.sourceNode] = 0;
-	
 
 	unsigned int *d_nodePointer;
 	unsigned int *d_edgeList;
 	unsigned int *d_dist;
-	PartPointer *d_partNodePointer; 
-	
+	PartPointer *d_partNodePointer;
+
 	bool finished;
 	bool *d_finished;
 
@@ -113,9 +105,13 @@ int main(int argc, char** argv)
 
 	gpuErrorcheck(cudaMallocManaged(&d_nodePointer, num_nodes * sizeof(unsigned int)));
 	gpuErrorcheck(cudaMallocManaged(&d_edgeList, (num_edges + num_nodes) * sizeof(unsigned int)));
-	
+
 	gpuErrorcheck(cudaMalloc(&d_finished, sizeof(bool)));
 	gpuErrorcheck(cudaMallocManaged(&d_partNodePointer, vGraph.numParts * sizeof(PartPointer)));
+
+	gpuErrorcheck(cudaMemAdvise(d_nodePointer, num_nodes * sizeof(uint), cudaMemAdviseSetPreferredLocation, arguments.deviceID));
+	gpuErrorcheck(cudaMemAdvise(d_edgeList, (num_edges + num_nodes)  * sizeof(uint), cudaMemAdviseSetPreferredLocation, arguments.deviceID));
+	gpuErrorcheck(cudaMemAdvise(d_partNodePointer, vGraph.numParts * sizeof(PartPointer), cudaMemAdviseSetPreferredLocation, arguments.deviceID));
 
 	memcpy(d_nodePointer, vGraph.nodePointer, num_nodes * sizeof(unsigned int));
 	memcpy(d_edgeList, vGraph.edgeList, (num_edges + num_nodes) * sizeof(unsigned int));
@@ -131,56 +127,53 @@ int main(int argc, char** argv)
 		itr++;
 		finished = true;
 		gpuErrorcheck(cudaMemcpy(d_finished, &finished, sizeof(bool), cudaMemcpyHostToDevice));
-		if(itr % 2 == 1)
+		if (itr % 2 == 1)
 		{
-			kernel<<< vGraph.numParts/512 + 1 , 512 >>>(vGraph.numParts, 
-														d_nodePointer,
-														d_partNodePointer,
-														d_edgeList, 
-														d_dist, 
-														d_finished,
-														level);
+			kernel<<<vGraph.numParts / 512 + 1, 512>>>(vGraph.numParts,
+													   d_nodePointer,
+													   d_partNodePointer,
+													   d_edgeList,
+													   d_dist,
+													   d_finished,
+													   level);
 		}
 		else
 		{
-			kernel<<< vGraph.numParts/512 + 1 , 512 >>>(vGraph.numParts, 
-														d_nodePointer, 
-														d_partNodePointer,
-														d_edgeList, 
-														d_dist, 
-														d_finished,
-														level);													
+			kernel<<<vGraph.numParts / 512 + 1, 512>>>(vGraph.numParts,
+													   d_nodePointer,
+													   d_partNodePointer,
+													   d_edgeList,
+													   d_dist,
+													   d_finished,
+													   level);
 		}
-	
-		gpuErrorcheck( cudaPeekAtLastError() );
-		gpuErrorcheck( cudaDeviceSynchronize() );
-		
+
+		gpuErrorcheck(cudaPeekAtLastError());
+		gpuErrorcheck(cudaDeviceSynchronize());
+
 		gpuErrorcheck(cudaMemcpy(&finished, d_finished, sizeof(bool), cudaMemcpyDeviceToHost));
-		
+
 		level++;
-		
+
 	} while (!(finished));
-	
+
 	cout << "Number of iterations = " << itr << endl;
 
 	float runtime = t.Finish();
 	cout << "Processing finished in " << runtime << " (ms).\n";
 
 	cout << "Total time in " << t3.Finish() << " (ms).\n";
-		
-	
-	gpuErrorcheck(cudaMemcpy(dist, d_dist, num_nodes*sizeof(unsigned int), cudaMemcpyDeviceToHost));
+
+	gpuErrorcheck(cudaMemcpy(dist, d_dist, num_nodes * sizeof(unsigned int), cudaMemcpyDeviceToHost));
 
 	utilities::PrintResults(dist, 30);
-	
-	if(arguments.hasOutput)
+
+	if (arguments.hasOutput)
 		utilities::SaveResults(arguments.output, dist, num_nodes);
-	
 
 	gpuErrorcheck(cudaFree(d_nodePointer));
 	gpuErrorcheck(cudaFree(d_edgeList));
 	gpuErrorcheck(cudaFree(d_dist));
 	gpuErrorcheck(cudaFree(d_finished));
 	gpuErrorcheck(cudaFree(d_partNodePointer));
-
 }
